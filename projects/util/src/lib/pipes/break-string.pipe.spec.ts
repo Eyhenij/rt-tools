388f077c554dee0c28b4c17866ccd790e4f3adf1
Yauheni Krumin <Eyhenij@gmail.com>
2024-01-14 15:09:51 +0700

    feat: add sanitize and break-string pipes

---
+++ projects/util/src/lib/pipes/break-string.pipe.spec.ts
@@ -0,0 +1,41 @@
+import { BreakStringPipe } from './break-string.pipe';
+
+
+describe(BreakStringPipe.name, () => {
+    const pipe: BreakStringPipe = new BreakStringPipe();
+
+    describe('when string contains capital letter', () => {
+        it('should return string with a breaks', () => {
+            const value: string = 'WithCapitalLetter';
+            expect(pipe.transform(value)).toBe('With Capital Letter');
+        });
+    });
+
+    describe('when string contains number', () => {
+        it('should return original string', () => {
+            const value: string = 'Withnumber6';
+            expect(pipe.transform(value)).toBe('Withnumber6');
+        });
+    });
+
+    describe('when string contains punctuation marks', () => {
+        it('should return original string', () => {
+            const value: string = 'Withpunctuationmarks!';
+            expect(pipe.transform(value)).toBe('Withpunctuationmarks!');
+        });
+    });
+
+    describe('when string does not contain capital letter', () => {
+        it('should return original string', () => {
+            const value: string = 'Withoutcapitalletter';
+            expect(pipe.transform(value)).toBe('Withoutcapitalletter');
+        });
+    });
+
+    describe('when string contains capital letter', () => {
+        it('should not return original string', () => {
+            const value: string = 'WithCapitalLetter';
+            expect(pipe.transform(value)).not.toBe('WithCapitalLetter');
+        });
+    });
+});
