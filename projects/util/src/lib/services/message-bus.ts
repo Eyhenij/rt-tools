ab65f93a1264fd5207c34f44b5c72117520b96f8
Yauheni Krumin <Eyhenij@gmail.com>
2024-01-21 11:31:57 +0700

    feat: add message bus

---
+++ projects/util/src/lib/services/message-bus.ts
@@ -0,0 +1,26 @@
+import { Observable, Subject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+
+export interface MessageBusEvent<T = string> {
+    readonly type: T;
+}
+
+export class MessageBus<T extends MessageBusEvent<M>, M> {
+    #eventSource: Subject<T> = new Subject<T>();
+
+    public emit(event: T): void {
+        this.#eventSource.next(event);
+    }
+
+    public onEmit(): Observable<T> {
+        return this.#eventSource.asObservable();
+    }
+
+    public ofType(eventType: M): Observable<T> {
+        return this.onEmit().pipe(
+            filter((event: T): boolean => event.type === eventType),
+            map((event: T) => event)
+        );
+    }
+}
