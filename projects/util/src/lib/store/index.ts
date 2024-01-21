6d14712eaeed28516d3b4fc6bde4c393ec1022d1
Yauheni Krumin <Eyhenij@gmail.com>
2024-01-21 12:23:07 +0700

    feat: add exports for store interfaces

--- projects/util/src/lib/store/index.ts
+++ projects/util/src/lib/store/index.ts
@@ -3,6 +3,8 @@ export * from './base-async-store.service';
 
 // interfaces
 export * from './interfaces/action.interface';
+export * from './interfaces/async-store-service.model';
+export * from './interfaces/entities-state.model';
 
 // enums
 export * from './enums/async-state-status.enum';
