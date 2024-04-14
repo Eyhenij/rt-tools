62c1b7284f827de95b2e702d4008ca28de9ac645
Yauheni Krumin <Eyhenij@gmail.com>
2024-04-14 15:19:37 +0700

    chore: update BreakpointService

--- projects/util/src/lib/services/breakpoints.service.ts
+++ projects/util/src/lib/services/breakpoints.service.ts
@@ -19,25 +19,29 @@ export class BreakpointService {
     readonly #smallTabletQuery: string = `(min-width: ${this.#notIncludeValue(this.#breakpoints.sm)})`;
     readonly #mobileQuery: string = `(max-width: ${this.#notIncludeValue(this.#breakpoints.xs)})`;
 
-    public readonly isDesktopSource: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
+    public readonly isDesktop: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
         this.#breakpointObserver.observe(this.#desktopQuery).pipe(map(({ matches }: BreakpointState) => matches))
     );
 
-    public readonly isSmallDesktopSource: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
+    public readonly isSmallDesktop: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
         this.#breakpointObserver.observe(this.#smallDesktopQuery).pipe(map(({ matches }: BreakpointState) => matches))
     );
 
-    public readonly isMobileSource: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
+    public readonly isMobile: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
         this.#breakpointObserver.observe(this.#mobileQuery).pipe(map(({ matches }: BreakpointState) => matches))
     );
 
-    public readonly isTabletSource: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
+    public readonly isTablet: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
         this.#breakpointObserver.observe(this.#tabletQuery).pipe(map(({ matches }: BreakpointState) => !matches))
     );
 
-    public readonly isSmallTabletSource: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
+    public readonly isSmallTablet: Signal<Nullable<boolean>> = toSignal<Nullable<boolean>>(
         this.#breakpointObserver.observe(this.#smallTabletQuery).pipe(map(({ matches }: BreakpointState) => matches))
     );
+
+    public setBreakpoints(breakpoints: IBreakpoints): void {
+        Object.assign(this.#breakpoints, breakpoints);
+    }
 
     #notIncludeValue(value: string): string {
         return Number(value.split('px')[0]) - 1 + 'px';
