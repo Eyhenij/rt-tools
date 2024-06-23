d843caf0690434f78593ecab79e752bfd043b61c
Yauheni Krumin <72300075+Eyhenij@users.noreply.github.com>
2024-06-23 19:20:18 +0300

    lint: add stylelint (#6)

--- projects/util/.storybook/preview.ts
+++ projects/util/.storybook/preview.ts
@@ -1,14 +1,14 @@
-import type { Preview } from "@storybook/angular";
+import type { Preview } from '@storybook/angular';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
---
+++ stylelint.config.js
@@ -0,0 +1,38 @@
+export default {
+    extends: ['stylelint-config-standard', './node_modules/prettier-stylelint/config.js', 'stylelint-config-idiomatic-order'],
+    plugins: ['stylelint-scss', 'stylelint-prettier'],
+    rules: {
+        'at-rule-empty-line-before': [
+            'always',
+            {
+                ignore: ['after-comment', 'blockless-after-same-name-blockless', 'first-nested'],
+                ignoreAtRules: ['else'],
+            },
+        ],
+        'at-rule-no-unknown': null,
+        'function-name-case': [
+            'lower',
+            {
+                ignoreFunctions: ['/^get.*$/'],
+            },
+        ],
+        'no-descending-specificity': null,
+        'rule-empty-line-before': [
+            'always',
+            {
+                except: ['first-nested'],
+                ignore: ['after-comment'],
+            },
+        ],
+        'scss/at-rule-no-unknown': true,
+        'selector-pseudo-element-colon-notation': 'single',
+        'selector-pseudo-element-no-unknown': [
+            true,
+            {
+                ignorePseudoElements: ['ng-deep'],
+            },
+        ],
+        'no-invalid-position-at-import-rule': null,
+        'prettier/prettier': true,
+    },
+};
