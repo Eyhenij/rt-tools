da341e3046912d3f00226734ccae77c6e34d167d
Yauheni Krumin <Eyhenij@gmail.com>
2024-06-26 18:12:13 +0700

    fix: rollback exports

--- projects/util/src/lib/bem/index.ts
+++ projects/util/src/lib/bem/index.ts
@@ -1,1 +1,6 @@
-export * from './public-api';
+export * from './bem.types';
+
+export * from './block.directive';
+export * from './concat-classes.pipe';
+export * from './elem.directive';
+export * from './mod.directive';
--- projects/util/src/lib/bem/public-api.ts
+++
@@ -1,6 +0,0 @@
-export * from './bem.types';
-
-export * from './block.directive';
-export * from './concat-classes.pipe';
-export * from './elem.directive';
-export * from './mod.directive';
