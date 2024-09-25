be7b759c65b1de1aa9602ba5d1726c1f50d0c8dc
SVIATASLAU KHUTARNY <113117198+treble3d@users.noreply.github.com>
2024-09-25 09:09:00 +0300

    style(rt:ui-kit): change dynamic selector styles (#82)

--- projects/tools/src/lib/ui-kit/dynamic-selectors/components/index.ts
+++ projects/tools/src/lib/ui-kit/dynamic-selectors/components/index.ts
@@ -1,4 +1,5 @@
-export * from './multi-selector-popup/rtui-multi-selector-popup.component';
 export * from './dynamic-selector/rtui-dynamic-selector.component';
+export * from './dynamic-input/rtui-dynamic-input.component';
+export * from './multi-selector-popup/rtui-multi-selector-popup.component';
 export * from './placeholder/rtui-dynamic-selector-placeholder.component';
 export * from './actions/rtui-dynamic-selector-list-actions.component';
