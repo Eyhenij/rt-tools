1f08c2349ae7afd303d9020955a952279ffa11c1
Yauheni Krumin <72300075+Eyhenij@users.noreply.github.com>
2024-11-22 07:13:51 +0700

    fix: empty to dash pipe (#128)

--- projects/tools/src/lib/util/functions/empty-to-dash.ts
+++ projects/tools/src/lib/util/functions/empty-to-dash.ts
@@ -2,5 +2,5 @@ import { DASH } from '../const';
 import { isNil } from './is-nil';
 
 export function emptyToDash<T>(value: T | null | undefined): T | string {
-    return isNil(value) ? DASH : value;
+    return isNil(value) || value === '' ? DASH : value;
 }
