7f1a836a69c0301090f9508adfef553fb247c7f1
SVIATASLAU KHUTARNY <sviathut@gmail.com>
2024-11-29 09:46:42 +0200

    feat(rt:ui-kit): implement image uploader

---
+++ projects/tools/src/lib/ui-kit/image-uploader/stories/component/test-image-upload.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { RtuiImageUploadComponent } from '../../image-uploader/rtui-image-upload.component';
+
+@Component({
+    standalone: true,
+    selector: 'app-image-upload',
+    templateUrl: './test-image-upload.component.html',
+    styleUrls: ['./test-image-upload.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        // components
+        RtuiImageUploadComponent,
+    ],
+})
+export class TestImageUploadComponent {
+    public imageUrl: string = '';
+
+    public imageChanged(file: File): void {
+        // eslint-disable-next-line no-console
+        console.log('image changed:', file);
+    }
+}
---
+++ projects/tools/src/lib/ui-kit/image-uploader/stories/upload.stories.ts
@@ -0,0 +1,22 @@
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { faker } from '@faker-js/faker';
+import { Meta, StoryObj, applicationConfig } from '@storybook/angular';
+
+import { TestImageUploadComponent } from './component/test-image-upload.component';
+
+export default {
+    title: 'Components/ImageUpload',
+    component: TestImageUploadComponent,
+    decorators: [
+        applicationConfig({
+            providers: [provideAnimations()],
+        }),
+    ],
+} as Meta<TestImageUploadComponent>;
+
+type Story = StoryObj<TestImageUploadComponent>;
+
+export const ImageUpload: Story = {
+    args: { imageUrl: faker.image.url({ width: 200, height: 200 }) },
+};
