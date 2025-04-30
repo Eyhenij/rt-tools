e5566b8215818d9027b582b26327d165a15991c6
SVIATASLAU KHUTARNY <sviathut@gmail.com>
2025-04-30 12:07:57 +0300

    feat: add edit field option for dynamic input

--- projects/tools/src/lib/ui-kit/dynamic-selectors/components/selected-list/rtui-dynamic-selector-selected-list.component.ts
+++ projects/tools/src/lib/ui-kit/dynamic-selectors/components/selected-list/rtui-dynamic-selector-selected-list.component.ts
@@ -11,10 +11,13 @@ import {
     InputSignalWithTransform,
     output,
     OutputEmitterRef,
+    signal,
     Signal,
     TemplateRef,
+    viewChild,
+    WritableSignal,
 } from '@angular/core';
-import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 
@@ -27,6 +30,10 @@ import {
     RtIconOutlinedDirective,
     transformArrayInput,
 } from '../../../../util';
+import { BooleanInput } from '@angular/cdk/coercion';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatInput } from '@angular/material/input';
+import { MatFormFieldAppearance } from '@angular/material/form-field';
 
 /** Directive for row actions located outside a row menu button */
 @Directive({
@@ -40,12 +47,15 @@ export class RtuiDynamicSelectorItemAdditionalControlDirective {}
     styleUrls: ['./rtui-dynamic-selector-selected-list.component.scss'],
     imports: [
         NgTemplateOutlet,
+        FormsModule,
+        ReactiveFormsModule,
 
         // material
         MatIconButton,
         MatIcon,
-        MatButton,
         MatTooltip,
+        MatInput,
+        MatFormField,
 
         // drag and drop
         CdkDropList,
@@ -66,13 +76,17 @@ export class RtuiDynamicSelectorItemAdditionalControlDirective {}
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RtuiDynamicSelectorSelectedListComponent<ENTITY extends Record<string, unknown>, KEY extends Extract<keyof ENTITY, string>> {
+    protected readonly editedItemIndex: WritableSignal<Nullable<number>> = signal(null);
+
+    public readonly inputRef: Signal<Nullable<MatInput>> = viewChild(MatInput);
+
     /** Indicates if mobile view */
-    public isMobile: InputSignalWithTransform<Nullable<boolean>, Nullable<boolean>> = input.required<Nullable<boolean>, Nullable<boolean>>({
+    public isMobile: InputSignalWithTransform<boolean, BooleanInput> = input.required<boolean, BooleanInput>({
         transform: booleanAttribute,
     });
-    /** A model's field which should be used for http-requests */
+    /** A model's field, which should be used for http-requests */
     public keyExp: InputSignal<KEY> = input.required();
-    /** A model's field which should be shown in ui */
+    /** A model's field, which should be shown in ui */
     public displayExp: InputSignal<KEY> = input.required();
     /** Array of selected entities */
     public selectedEntities: InputSignalWithTransform<ENTITY[], ENTITY[]> = input.required<ENTITY[], ENTITY[]>({
@@ -82,22 +96,32 @@ export class RtuiDynamicSelectorSelectedListComponent<ENTITY extends Record<stri
     public readonlyEntitiesKeys: InputSignalWithTransform<ENTITY[KEY][], ENTITY[KEY][]> = input<ENTITY[KEY][], ENTITY[KEY][]>([], {
         transform: (value: ENTITY[KEY][]) => transformArrayInput(value),
     });
-    /** Indicates is list of items draggable */
-    public isListDraggable: InputSignalWithTransform<boolean, boolean> = input<boolean, boolean>(false, {
+    /** Indicates is a list of items draggable */
+    public isListDraggable: InputSignalWithTransform<boolean, BooleanInput> = input<boolean, BooleanInput>(false, {
         transform: booleanAttribute,
     });
     /** Indicates is break string pipe used */
-    public useNameBreaking: InputSignalWithTransform<boolean, boolean> = input<boolean, boolean>(false, {
+    public useNameBreaking: InputSignalWithTransform<boolean, BooleanInput> = input<boolean, BooleanInput>(false, {
         transform: booleanAttribute,
     });
     /** Indicates is title case pipe used */
-    public useTitleCase: InputSignalWithTransform<boolean, boolean> = input<boolean, boolean>(false, {
+    public useTitleCase: InputSignalWithTransform<boolean, BooleanInput> = input<boolean, BooleanInput>(false, {
         transform: booleanAttribute,
     });
-    /** Indicates is delete entity button from the selected list shown */
-    public isDeleteButtonShown: InputSignalWithTransform<boolean, boolean> = input<boolean, boolean>(true, {
+    /** Indicates is deleting entity button from the selected list shown */
+    public isDeleteButtonShown: InputSignalWithTransform<boolean, BooleanInput> = input<boolean, BooleanInput>(true, {
         transform: booleanAttribute,
     });
+    /** Indicates is items editable */
+    public isItemsEditable: InputSignalWithTransform<boolean, BooleanInput> = input<boolean, BooleanInput>(false, {
+        transform: booleanAttribute,
+    });
+    /** Material elements appearance */
+    public appearance: InputSignal<MatFormFieldAppearance> = input('fill' as MatFormFieldAppearance);
+
+    public readonly deleteFromSelectedAction: OutputEmitterRef<ENTITY[KEY]> = output<ENTITY[KEY]>();
+    public readonly dropAction: OutputEmitterRef<CdkDragDrop<ENTITY[]>> = output<CdkDragDrop<ENTITY[]>>();
+    public readonly changeValueAction: OutputEmitterRef<{ prev: ENTITY[KEY]; new: string }> = output<{ prev: ENTITY[KEY]; new: string }>();
 
     /** Additional control for entity */
     public readonly additionalControlTpl: Signal<Nullable<TemplateRef<{ $implicit: ENTITY }>>> = contentChild(
@@ -107,16 +131,22 @@ export class RtuiDynamicSelectorSelectedListComponent<ENTITY extends Record<stri
         }
     );
 
-    public readonly deleteFromSelectedAction: OutputEmitterRef<ENTITY[KEY]> = output<ENTITY[KEY]>();
-    public readonly dropAction: OutputEmitterRef<CdkDragDrop<ENTITY[]>> = output<CdkDragDrop<ENTITY[]>>();
-
-    public onDelete(value: ENTITY[KEY]): void {
+    protected onDelete(value: ENTITY[KEY]): void {
         this.deleteFromSelectedAction.emit(value);
     }
 
-    public onDrop(event: CdkDragDrop<ENTITY[]>): void {
+    protected onDrop(event: CdkDragDrop<ENTITY[]>): void {
         if (event.currentIndex !== event.previousIndex) {
             this.dropAction.emit(event);
         }
+    }
+
+    protected changeValue(prev: ENTITY[KEY]): void {
+        this.changeValueAction.emit({ prev, new: this.inputRef()?.value || '' });
+        this.setEditModState(null);
+    }
+
+    protected setEditModState(index: Nullable<number>): void {
+        this.editedItemIndex.set(index);
     }
 }
